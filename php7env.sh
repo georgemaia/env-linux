817d38c5450f19a8cc5fc1210ed9847a1cd57573
George Maia <georgemaia@users.noreply.github.com>
2019-10-03 10:57:04 -0300

    added php-mbstring php-gettext

--- php7env.sh
+++ php7env.sh
@@ -49,7 +49,7 @@ php7.3-pgsql php7.3-sqlite3 php7.3-gd \
 php7.3-curl php7.3-memcached \
 php7.3-imap php7.3-mysql php7.3-mbstring \
 php7.3-xml php7.3-zip php7.3-bcmath php7.3-soap \
-php7.3-intl php7.3-readline
+php7.3-intl php7.3-readline php-mbstring php-gettext
 
 # Install Composer
 curl -sS https://getcomposer.org/installer | php
